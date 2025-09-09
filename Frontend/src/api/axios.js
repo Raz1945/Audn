066e1d65cd6fb1efbef206a28bd41164ac46320f
raz1945 <g.sebas.sanchez@gmail.com>
2025-09-08 23:08:52 -0300

    go7 conect

--- Frontend/src/api/axios.js
+++ Frontend/src/api/axios.js
@@ -1,6 +1,12 @@
 import axios from 'axios';
 
+// Detecta si estamos en producción o local
+const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000';
+
 // Configuración Global de  Axios, ayuda a reducir la redundancia en tus solicitudes.
 export default axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: BASE_URL,
+  withCredentials: true // si tu backend usa cookies
+  // baseURL: 'http://localhost:3000',
 });
+
