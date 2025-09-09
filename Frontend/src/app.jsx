65bb78db0f1a496f58dec2e0a4a3e68e88c4ca6b
raz1945 <g.sebas.sanchez@gmail.com>
2025-09-08 21:33:04 -0300

    correcciones para build

--- Frontend/src/app.jsx
+++ Frontend/src/app.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
-import {store} from './app/store'
 import { RouterProvider } from "react-router-dom";
-import { AppRoutes } from './routes/AppRoutes';
+import { Provider } from 'react-redux';
+import {store} from '@app/store'
+import { AppRoutes } from '@routes/AppRoutes';
 
 import './app.css'
 
